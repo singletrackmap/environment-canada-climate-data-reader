3411e94188a8454cf33e55b9ece264a1abc2f3a6
hawklorry <hawklorry@gmail.com>
2014-01-03 14:50:08 +0000

    From University Office Add function to retrieve Australia climate data from BOM (Bureau of Meteorology)

---
+++ EnvironmentCanadaClimateData/Australia/FrmAustralia.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HAWKLORRY.Australia
+{
+    public partial class FrmAustralia : Form
+    {
+        public FrmAustralia()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StationAustralia s = new StationAustralia("");
+            this.richTextBox1.Text = s.retrieveDailyOneTypeClimateData(ClimateDataType.PRECIPITATION);
+        }
+    }
+}
